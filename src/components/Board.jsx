cf6edda1d499a8752d595f039bb536ceb5e0023f
unknown <vipin.19b131027@abes.ac.in>
2022-07-12 18:18:50 +0530

    events added

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,27 +1,59 @@
-import React from 'react'
-import Square from './Square'
+import React, { useState } from 'react';
+import Square from './Square';
 
 const Board = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [isXNext,setIsNext]=useState(false);
+
+  const handleSquareClick = (position) => {
+    
+    if( board[position] ){
+      return;
+    }
+    
+    setBoard(prev => {
+      return prev.map((square, pos) => {
+        if (pos === position) {
+          return isXNext ? 'X':'O';
+        }
+
+        return square;
+      });
+    });
+
+
+    setIsNext(  (prev) => !prev);
+  };
+// rendersquare will give value to a single square whenever called
+  const renderSquare = position => {
+    return (
+      <Square
+        value={board[position]}
+        onClick={() => handleSquareClick(position)}
+      />
+    );
+  };
+
+  // Board.jsx is finally returning this
   return (
-    <div className='board'>
-        <div className='board-row'>
-            <Square value={0}/>
-            <Square value={1}/>
-            <Square value={2}/>
-        </div>
-        <div className='board-row'>
-            <Square value={3}/>
-            <Square value={4}/>
-            <Square value={5}/>
-        </div>
-        <div className='board-row'>
-            <Square value={6}/>
-            <Square value={7}/>
-            <Square value={8}/>
-        </div>
-        
+    <div className="board">
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
     </div>
   );
 };
 
-export default Board
+export default Board;
